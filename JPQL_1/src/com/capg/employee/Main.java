e2b76710404d9b83ba20483b803fd908e14c6402
Syed Ajaz Haider Zaidi <syedajaz.zaidi@gmail.com>
2019-02-21 11:04:02 +0530

    Update Main.java

--- JPQL_1/src/com/capg/employee/Main.java
+++ JPQL_1/src/com/capg/employee/Main.java
@@ -13,15 +13,23 @@ public class Main {
 		EntityManagerFactory emfactory = Persistence.createEntityManagerFactory("Employee_Details");
 		EntityManager entitymanager = emfactory.createEntityManager();
 
-		// MAX SALARY
+		
 		Query query1 = entitymanager.createQuery("Select MAX(e.salary) from Employee e");
 		Double result = (Double) query1.getSingleResult();
 		System.out.println("Max Employee Salary :" + result);
 
+		 Query query2 = entitymanager.createQuery( "Select e " + "from Employee e " + "where e.salary " + "Between 30000 and 40000" );
+      
+      List<Employee> list=(List<Employee>)query.getResultList( );
+
+      for( Employee e:list ){
+         System.out.print("Employee ID :" + e.getEid( ));
+         System.out.println("\t Employee salary :" + e.getSalary( ));
+      }
+
 		
 		
 		
-		// PRINT ALL EMPLOYEE
 		Query query = entitymanager.createQuery( "Select e " + "from Employee e " );
 	      
 	      List<Employee> list=(List<Employee>)query.getResultList( );
